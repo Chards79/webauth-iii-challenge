63f7e570799b50b19c8fb572339628b4e532c614
Eric Richards <esholundr@yahoo.com>
2019-12-19 16:22:15 -0700

    changed up restricted-middleware to make GET endpoint work with proper authorization

--- auth/restricted-middleware.js
+++ auth/restricted-middleware.js
@@ -1,24 +1,20 @@
-const bcrypt = require('bcryptjs');
-
-const Users = require('../users/users-model');
+const jwt = require('jsonwebtoken');
+const secrets = require('../config/secrets');
 
 module.exports = (req, res, next) => {
-    const { username, password } = req.headers;
+    const { authorization } = req.headers;
 
-    if (username && password) {
-        Users.findBy({ username })
-            .first()
-            .then(user => {
-                if (user && bcrypt.compareSync(password, user.password)) {
-                    next();
-                } else {
-                    res.status(401).json({ message: 'You shall not pass' });
-                }
-            })
-            .catch(error => {
-                res.status(500).json({ message: 'Ran into an unexpected error', error });
-            });
+    if (authorization) {
+
+        jwt.verify(authorization, secrets.jwtSecret, function (err, decodedToken) {
+            if (err) {
+                res.status(401).json({ message: 'You shall not pass' });
+            } else {
+                req.token = decodedToken;
+                next();
+            }
+        });
     } else {
-        res.status(400).json({ message: 'You shall not pass' });
+        res.status(400).json({ message: 'Please login and try again' });
     }
 };
